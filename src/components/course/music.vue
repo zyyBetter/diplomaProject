62c68eae7bb9e6f6d1f03d30478cf3b099172c57
zyy <zyy101410@163.com>
2018-02-24 15:40:47 +0800

    课程页面头部的搭建

--- src/components/course/music.vue
+++ src/components/course/music.vue
@@ -1,11 +1,49 @@
 <template>
  <div id="temp">
+   <!--头部-->
+   <header>
+     <span>课程列表</span>
+     <div>
+       <span>查看班级</span>
+       <span>查看公开课</span>
+     </div>
+   </header>
+   <section id="title">
+     <ul class="title_01">
+       <li>所有的分类:
+         <ul class="title_02">
+           <li>童谣</li>
+           <li>故事</li>
+           <li>手工制作</li>
+           <li>书法</li>
+           <li>故事</li>
+           <li>橡树鸟</li>
+         </ul></li>
+       <li>
+         分类:
+         <ul class="title_02">
+           <li>童谣</li>
+           <li>故事</li>
+           <li>手工制作</li>
+           <!--<li>书法</li>-->
+           <!--<li>故事</li>-->
+           <!--<li>橡树鸟</li>-->
+         </ul>
+       </li>
+       <li>专题:
+         <ul class="title_02">
+           <li>防火</li>
+           <li>新年</li>
+           <li>叮当</li>
+           <li>早活动</li>
+           <li>故事</li>
+         </ul></li>
+       <li>科目:</li>
+     </ul>
+   </section>
 
-{{msg}}
 
 
-
-<div id="ca"></div>
 
 
  </div>
@@ -35,5 +73,69 @@
 </script>
 
 <style scoped>
+header{
+  width: 100%;
+  height: 80px;
+}
+header>span{
+  display: inline-block;
+  width: 200px;
+  height: 80px;
+  float: left;
+  line-height:60px;
+  font-size: 20px;
+  font-weight:bolder;
+  color: #37b97d;
+}
+header div span{
+  display: inline-block;
+  width: 150px;
+  height: 45px;
+  background: #37b97d;
+  float: right;
+  margin-right: 30px;
+  margin-top: 20px;
+  border-radius: 8px;
+  line-height:45px;
+  color: white;
+}
+  #title{
+    width: 100%;
+    height: 200px;
+    /*background: darkgoldenrod;*/
+  }
+#title .title_01{
+  width: 100%;
+  /*height: 100%;*/
+  overflow: hidden;
+  border: 1px solid silver;
+}
+#title .title_01>li {
+  border-bottom: 1px solid silver;
+
+}
+#title .title_01 li {
+  /*width: 100%;*/
+  height: 50px;
+  /*border: 1px solid silver;*/
+  text-align: left;
+  padding-left: 60px;
+  line-height: 50px;
+}
+#title .title_01 .title_02{
+  width: 100%;
+  overflow: hidden;
+}
+#title .title_01 .title_02 li:hover {
+  color: #37b97d;
+}
+#title .title_01 .title_02 li {
+  cursor: pointer;
+  float: left;
+  width: 100px;
+  margin-left: 10px;
+  /*background: seagreen;*/
+
+}
 
 </style>
--- src/components/subject/fire.vue
+++ src/components/subject/fire.vue
@@ -18,6 +18,12 @@
 
    </section>
 
+   <!--<article>-->
+     <div id="box">
+
+     </div>
+   <!--</article>-->
+
 
 
 
@@ -26,6 +32,10 @@
 </template>
 
 <script>
+//  import '../../../static/js/underscore.js';
+
+
+
 
 
   export default {
@@ -34,10 +44,203 @@
       msg:"fire"
     }
   },
-  created:function (){
 
+  mounted:function (){
+//    this.getimg();
   },
     methods:{
+  /*  getimg(){
+
+      //1.0
+      function $(id){
+        return document.getElementById(id);
+      }
+
+      //2.0
+      function getTagName(obj,tagName)
+      {
+        return obj.getByTagName(tagName);
+      }
+
+      //3.0
+      function scroll(){
+        //最新浏览器,IE9+
+        if(window.pageXOffset||window.pageYOffset)
+        {
+          return {
+            top:window.pageYOffset,
+            left:window.pageXOffset        }}
+        //标准模式
+        else if(document.compatMode=="css1Compat"){
+          return {
+            top:document.documentElement.scrollTop,
+            left:document.documentElement.scrollLeft
+          }
+        }
+        //怪异模式
+        else{
+          return {
+            top: document.body.scrollTop,
+            left: document.body.scrollLeft
+          }
+        }
+
+      }
+
+     /!* /!**
+       * client() 获取屏幕的宽度
+       * 使用方法 client().width
+       * @returns {*}   屏幕的宽度及高度
+       *!/!*!/
+      function client(){
+        if(window.innerHeight||window.innerWidth)//IE9+ 及最新浏览器
+        {
+          return{
+            height:window.innerHeight,
+            width:window.innerWidth
+          }
+        }
+        else if(document.compatMode=="css1compat"){  //标准浏览器
+          return {
+            height:document.documentElement.clientHeight,
+            width:document.documentElement.clientWidth
+
+          }}
+        else
+        {
+          return { //怪异浏览器
+            height:document.body.clientHeight,
+            width:document.body.clientWidth
+
+          }
+        }
+      }
+
+
+      function waterfall(){
+        var boxChild=box.children;
+        var screenW=client().width;//计算网页的宽度
+        var box1W=box1.offsetWidth;
+        var cols=parseInt(screenW/box1W);//计算网页的宽度可以放多少张图片
+        var boxW=cols*box1W;//计算大盒子的宽度
+        // console.log(boxW);
+        box.style.width=boxW+"px";//给大盒子设置宽度
+        box.style.margin="0 auto";//让大盒子居中
+
+
+        //给每张照片定位
+        var arr=[];
+        for(var j=0;j<boxChild.length;j++){
+          var boxChildH=boxChild[j].offsetHeight;//获取每一个盒子的高度
+          //当照片在第一排的时候把盒子高度直接放进数组里面
+          if(j<cols) {
+            arr.push(boxChildH);
+            boxChild[j].style.position="absolute";//盒子定位
+            boxChild[j].style.top=0+"px";
+            boxChild[j].style.left=boxChild[j].offsetWidth*j+"px";
+
+
+
+          }
+          else{
+            var minH=_.min(arr);  //1找出数组里面的最小的数,利用函数库
+
+            // console.log(minH);
+            var indexMin=arr.indexOf(minH);//找出最小数的索引indexOf,方便把下一个盒子定位
+            // console.log(indexMin);
+            boxChild[j].style.position="absolute";//盒子定位
+            boxChild[j].style.top=minH+"px";
+            boxChild[j].style.left=boxChild[j].offsetWidth*indexMin+"px";
+            arr[indexMin]=boxChildH+minH;//更新高度
+          }
+        }
+      }
+
+
+      window.onload=function (){ //页面加载即调用
+        waterfall();
+        window.onscroll=function (){  //页面滚动的时候调用
+
+          if(loaded()==true){ // 当loaded函数为真的时候执行下面的代码
+            // alert(1);
+
+            for(var i=0;i<20;i++){//循环20次,即每次加载20张照片
+
+              var box1=document.createElement("div");  //创建新的大盒子
+              box.appendChild(box1);
+              box1.className="box1";
+
+              var box2=document.createElement("div");//创建新的小盒子
+              box1.appendChild(box2);
+              box2.className="box2"
+
+              var pic=document.createElement("img");//创建新照片节点
+              box2.appendChild(pic);
+              pic.className="pic";
+
+              var a=parseInt(_.random(0,40));  //照片的随机数
+              // console.log(a);
+              pic.src="../../../static/img/pubu/"+a+".jpg"; //设置照片获取的路径
+
+
+            }
+            waterfall();
+
+          }
+
+        }
+      }
+
+      var timer=null;
+
+      window.onresize=function(){
+
+        //函数节流
+        clearTimeout(timer);
+        timer=setTimeout(function(){
+          waterfall();
+          console.log(1);
+        },100);
+
+
+      }
+
+      function loaded(){
+        var allbox=$("box").children;
+        var lastindex=allbox.length-1;//获取最后一个盒子的索引
+        // console.log(lastindex);
+        var screenY=client().height;//获取页面的高度
+        var scrollY=scroll().top;//获取页面滚动的距离
+        var lastH=allbox[lastindex].offsetTop;
+        var isloading=lastH<=(screenY+scrollY);
+        // console.log(isloading);
+        return isloading;
+      }
+
+//滚动就调用
+
+
+
+      var box=$("box");
+      for(var i=0;i<40;i++){
+        var box1=document.createElement("div");
+        //创建图片外面的最大的盒子,并添加属性
+        box1.className="box1";
+        box.appendChild(box1);
+        var box2=document.createElement("div");
+
+        //创建图片外面的最大的盒子,并添加属性
+        box2.className="box2";
+        box1.appendChild(box2);
+
+        //创建img标签,并添加属性,添加每张图片的路径
+        var pic=document.createElement("img");
+        pic.className="pic";
+        box2.appendChild(pic);
+        pic.src="../../../static/img/pubu/"+i+".jpg";
+      }
+
+    }*/
 
     }
 }
@@ -99,5 +302,30 @@
     font-weight:bolder;
     line-height:50px;
   }
+  article{
+    width: 100%;
+    height: 1000px;
+    margin-top: 30px;
+    background: pink;
+  }
 
+  #box{
+    position: relative;
+    background: deeppink;
+    width: 100%;
+    margin-top: 30px;
+  }
+  .box1{
+    float: left;
+    padding:15px 0 0 15px;
+  }
+  .box2{
+    padding:10px;
+    margin: 0 auto;
+    border:1px solid slateblue;
+    border-radius: 5px;
+  }
+  .pic{
+    width: 165px;
+  }
 </style>
