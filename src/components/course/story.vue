7276f040c9784b38884b2ea7fce2887814585b7d
unknown <zyy101410@163.com>
2018-03-30 00:59:04 +0800

    购物车模块完善

--- src/components/course/story.vue
+++ src/components/course/story.vue
@@ -2,11 +2,7 @@
  <div id="temp">
 <header>
   <ul>
-    <li><span></span>睡前故事</li>
-    <li><span></span>儿歌大全</li>
-    <li><span></span>公主歌</li>
-    <li><span></span>阶段儿歌</li>
-
+    <li v-for="item in arrNar" @click="getId(item.id)"><span></span>{{item.txt}}</li>
   </ul>
 </header>
 <section>
@@ -24,7 +20,7 @@
    <article class="clearfix"  style="margin-top: 20px;">
    <div style="float: left;width: 20%;background: papayawhip;">
      <span style=" float: left;display: inline-block;width: 30px;height: 30px;background: url('../../../static/img/lesson1/int.png') 0 -690px no-repeat"></span>
-     <span style="font-size: 18px;line-height: 30px;width: 100px;float: left;">猜你喜欢的</span>
+     <span style="font-size: 18px;line-height: 30px;width: 100px;float: left;">推荐</span>
      <ul class="clearfix">
        <li v-for="item in fav" style="width: 100%;padding: 10px;">
          <div style="float: left;"><img :src="item.src" alt=""></div>
@@ -38,7 +34,7 @@
    </div>
 
      <div class="article_right"   >
-       <div style="background: url('../../../static/img/lesson1/int6.png') 0px 0px no-repeat;width: 200px;height: 30px;background-size: 100%"></div>
+
        <ul>
          <li v-for="item in arrdetail">
            <img :src="item.src" alt="">
@@ -62,6 +58,12 @@
   export default {
   data () {
     return {
+      arrNar:[
+        {txt:"睡前故事",id:"sleep"},
+        {txt:"儿歌大全",id:"music"},
+        {txt:"公主歌",id:"gir"},
+        {txt:"阶段儿歌",id:"age"},
+      ],
       msg:"story",
       fav:[{
         src: "../../../static/img/fav/1.png", text1: "小苹果", text2: "中文儿歌",video:"http://s.61baobao.com//flash/2014/7/069.swf"
@@ -89,23 +91,23 @@
       sleep:{
         head:{src:"../../../../static/img/groom/1.jpg",title:"儿童睡前故事",detail:'月亮爬上树梢，星星不断眨眼，孩子却不肯睡觉，这可愁坏了爸妈。不如试试给孩子看看六一宝宝为您特别呈现的儿童睡前故事，让孩子伴随着唯美的童话故事，香甜入睡。'},
         num:[
-          {src:"../../../../static/img/groom/1.jpg",txt:"ss"}
+          {src:"../../../../static/img/groom/2.jpg",txt:"ss"}
         ]
       },
       music:{
-        head:{src:"../../../../static/img/groom/1.jpg",title:"儿童睡前故事",detail:'月亮爬上树梢，星星不断眨眼，孩子却不肯睡觉，这可愁坏了爸妈。不如试试给孩子看看六一宝宝为您特别呈现的儿童睡前故事，让孩子伴随着唯美的童话故事，香甜入睡。'},
+        head:{src:"../../../../static/img/groom/2.jpg",title:"儿歌大全100首 ",detail:'亲宝儿歌视频大全为小朋友们提供了最好看儿歌，全部都是小朋友们喜爱的经典儿歌，也都是亲宝网全力制作的新版亲宝儿歌，绝对好看绝对好听。'},
         num:[
           {src:"../../../../static/img/groom/1.jpg",txt:"ss"}
         ]
       },
       gir:{
-        head:{src:"../../../../static/img/groom/1.jpg",title:"儿童睡前故事",detail:'月亮爬上树梢，星星不断眨眼，孩子却不肯睡觉，这可愁坏了爸妈。不如试试给孩子看看六一宝宝为您特别呈现的儿童睡前故事，让孩子伴随着唯美的童话故事，香甜入睡。'},
+        head:{src:"../../../../static/img/groom/4.jpg",title:"起司公主儿歌",detail:'《起司公主》是由亲宝文化在2015年全新推出的公主系动画片，公主系列是亲宝专门为女宝宝打造的早教作品，作品将父母对女儿宛若公主般的疼爱与呵护化为设计的灵感，将高贵、精致、优雅的公主元素贯穿于整个系列中，亲宝相信“每个女孩都有一个公主梦”，每个女孩也都将成为公主。'},
         num:[
           {src:"../../../../static/img/groom/1.jpg",txt:"ss"}
         ]
       },
       age:{
-        head:{src:"../../../../static/img/groom/1.jpg",title:"儿童睡前故事",detail:'月亮爬上树梢，星星不断眨眼，孩子却不肯睡觉，这可愁坏了爸妈。不如试试给孩子看看六一宝宝为您特别呈现的儿童睡前故事，让孩子伴随着唯美的童话故事，香甜入睡。'},
+        head:{src:"../../../../static/img/groom/5.jpg",title:"0至3岁儿歌",detail:'所谓三岁看老，意思是0-3岁是宝宝早期教育的黄金时期。3岁以前对宝宝的教育决定了宝宝以后的性格及行为习惯的养成，此时，爸爸妈妈可以借助一些优良的儿歌载体，让宝宝多听一些有教育意义的儿歌视频，对宝宝的行为进行正确引导。0至3岁儿歌专题汇总了亲宝儿歌最受欢迎、最具教育意义、最有趣味性的儿歌。如果您喜欢，别忘了分享给更多拼音哦！'},
         num:[
           {src:"../../../../static/img/groom/1.jpg",txt:"ss"}
         ]
@@ -127,6 +129,11 @@
     console.log( this.arrhead)
   },
     methods:{
+      getId(id){
+        this.arrhead  = this.respon[id].head;
+        this.arrdetail  = this.respon[id].num;
+//        alert(id)
+      }
 
     }
 }
@@ -150,7 +157,8 @@
     height: 100%;
     border: 1px  solid saddlebrown;
     float: right;
-    line-height:60px;
+    line-height:40px;
+    padding-bottom: 20px;
     font-size: 18px;
     color:white
   }
@@ -162,36 +170,41 @@
     top: 6px;
   }
   header ul li:nth-child(1){
-    background: #2a99e5;
+    background: #49a5e5;
   }
   header ul li:nth-child(1) span{
     background: url("../../../static/img/lesson1/int.png") no-repeat -92px -269px;
+  }
+  header ul li:nth-child(1):hover span{
+    background: url("../../../static/img/lesson1/Rocket.gif") ;
   }
  header ul li:nth-child(2){
     background: #2fccae;
   }
   header ul li:nth-child(2) span{
-    background: url("../../../static/img/lesson1/int.png") no-repeat -191px -287px;
+    background: url("../../../static/img/lesson1/int.png") no-repeat -191px -288px;
+  }
+  header ul li:nth-child(2):hover span{
+    background: url("../../../static/img/lesson1/book.gif") ;
   }
  header ul li:nth-child(3){
     background: #f4903a;
   }
   header ul li:nth-child(3) span{
-    background: url("../../../static/img/lesson1/int.png") no-repeat 0px -268px;
+    background: url("../../../static/img/lesson1/int.png") no-repeat 0px -270px;
+  }
+  header ul li:nth-child(3):hover span{
+    background: url("../../../static/img/lesson1/cd.gif") ;
   }
   header ul li:nth-child(4){
     background: #ac6ec5;
   }
   header ul li:nth-child(4) span{
-    background: url("../../../static/img/lesson1/int.png") no-repeat -138px -270px;
+    background: url("../../../static/img/lesson1/int.png") no-repeat -138.5px -270px;
   }
-  header ul li:nth-child(5){
-    background: #49a5e5;
+  header ul li:nth-child(4):hover span{
+    background: url("../../../static/img/lesson1/game.gif") ;
   }
-  header ul li:nth-child(5) span{
-    background: url("../../../static/img/lesson1/int.png") no-repeat -92px -268px;
-  }
-
   section{
     margin-top: 30px;
     width: 100%;
